4299e812d6b882acdce499d56b405dee2802cb4d
Samuel5432 <62008635+Samuel5432@users.noreply.github.com>
2020-03-25 01:49:01 +0100

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,7 @@
+print("Hello. Welcome.")
+
+from math import pi
+
+r = float(input("Please, enter the value of your radius "))
+
+print ("The area of the circle with radius "+ str(r) + " is: " + str(pi * r**2))
